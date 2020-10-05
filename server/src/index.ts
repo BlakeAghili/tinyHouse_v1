a7a3ce5f0b186788e631b7f241f66a502bdae2d3
BlakeAghili <peopleofksmo@gmail.com>
2020-10-04 20:55:53 -0500

    First Commit

---
+++ server/src/index.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+//import {listings} from "./listings";
+//import bodyParser from "body-parser"
+import {ApolloServer} from "apollo-server-express";
+import {schema} from "./graphql";
+
+const app = express();
+const port = 9000;
+const server = new ApolloServer({schema});
+server.applyMiddleware({app, path:"/api"});
+
+//app.use(bodyParser.json());
+
+// REST API version.
+/*app.get('/listings', (_req, res) => {
+    res.send(listings);
+});
+
+app.post('/delete-listing', (req, res) => {
+   const id: string = req.body.id;
+   for(let i  = 0; i < listings.length; i++) {
+       if(listings[i].id == id) {
+           res.send(listings.splice(i, 1));
+       }
+   }
+   res.send('Could not delete this thing.');
+});*/
+
+
+
+app.listen(port);
+
+console.log('Fraiser is listening.');
